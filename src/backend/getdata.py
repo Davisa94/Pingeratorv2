6adde9a011a15c0d3d1ae456fb2a9bb1b59023d5
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-02 11:57:32 -0700

    added module for geting data from the db

---
+++ src/backend/getdata.py
@@ -0,0 +1,5 @@
+################################################################ 
+# This module will call a set of historical data from the DB
+# given a command line argument
+# Author: Austin Benitez
+################################################################
