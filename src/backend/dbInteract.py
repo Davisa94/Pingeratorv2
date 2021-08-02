8c51a1b20af6ae66bfa6737606840b673d79fe45
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-02 12:56:46 -0700

    added the db sql from the previous version

--- src/backend/dbInteract.py
+++ src/backend/dbInteract.py
@@ -2,3 +2,24 @@
 # A module for interacting with an existing MySQL Database
 # Author: Austin Benitez
 ################################################################
+
+from name import Name
+from location import Location
+from sectionDataClass import CourseSectionData
+import pymysql
+
+
+class DBInteract:
+    def __init__(self, dbObject=""):
+        self.DBO = dbObject
+
+
+    def getAllSpeeds(self):
+        sql = ("SELECT * FROM speeds")
+        self.DBO.cursor.execute(sql)
+        return dbObject
+
+    def getSomeSpeeds(self, start, end):
+        sql = ("SELECT * FROM speeds WHERE (course_code = %s and course_prefix = %s)")
+        self.DBO.cursor.execute(sql, (course_code, course_prefix))
+        return dbObject
