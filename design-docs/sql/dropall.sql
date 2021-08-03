33a6e23fb78a1cb349e48c9c3d6e88fd1552cb83
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-02 21:44:04 -0700

    renamed pings to ping

--- design-docs/sql/dropall.sql
+++ design-docs/sql/dropall.sql
@@ -5,6 +5,6 @@ set FOREIGN_KEY_CHECKS = 0;
 drop table if exists ipLookup;
 drop table if exists testTime;
 drop table if exists speed;
-drop table if exists pings;
+drop table if exists ping;
 
 set FOREIGN_KEY_CHECKS = 1;
